15c96eed646bc4346b57c8fea94bfee2fafafa65
cardoso-topdev <oscar@cardoso-devstar.com>
2023-04-30 05:07:33 -0600

    update a mainlayout

--- app/components/layouts/MainLayout.tsx
+++ app/components/layouts/MainLayout.tsx
@@ -3,15 +3,15 @@ import React, { ReactNode } from 'react';
 import { useState } from "react";
 import Header from '../Header';
 
-const MainLayout = ({children}: {children?: ReactNode}) => {
+const MainLayout = ({ children }: { children?: ReactNode }) => {
   const [isNavDrawerOpen, toggleNavDrawer] = useState(false)
   return (
     <>
       <main className="bg-backing">
         <div className="max-w-[1200px] m-auto">
-          <Header isNavDrawerOpen={isNavDrawerOpen} toggleNavDrawer={toggleNavDrawer}/>
-          {children}
+          <Header isNavDrawerOpen={isNavDrawerOpen} toggleNavDrawer={toggleNavDrawer} />
         </div>
+        {children}
       </main>
       {isNavDrawerOpen && (
         <div className="fixed top-0 left-0 w-[100vw] h-[100vh] bg-backing opacity-70 z-10 animation-overlay-fadein" onClick={() => toggleNavDrawer(!isNavDrawerOpen)}></div>
