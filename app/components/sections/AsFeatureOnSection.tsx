eaf56ae211b68bfeb67e5c984380b382cd87d2dc
cardoso-topdev <oscar@cardoso-devstar.com>
2023-05-01 09:01:37 -0600

    update AsFeatureOnSection component

--- app/components/sections/AsFeatureOnSection.tsx
+++ app/components/sections/AsFeatureOnSection.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const AsFeatureOnSection = () => {
   return (
     <section className='mt-[-100px] h-[420px] w-full bg-gradient-bottom-lime'>
-      <div className="max-w-[1200px] m-auto mt-[200px]">
+      <div className="max-w-[1200px] m-auto pt-[200px]">
         <div className="text-pale text-[24px] text-center font-bold">AS FEATURE ON</div>
         <div className="flex justify-between mt-[50px]">
           <Image
