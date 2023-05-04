61f5610c9d7785c0c1cc2f47e1949ec732398b60
cardoso-topdev <oscar@cardoso-devstar.com>
2023-05-03 23:45:23 -0600

    complete CoreFeaturesSection component for mobile responsive

--- app/components/sections/CoreFeaturesSection.tsx
+++ app/components/sections/CoreFeaturesSection.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 
 const CoreFeaturesSection = () => {
   return (
-    <section>
+    <section className="px-[30px]">
       <div className="max-w-[1200px] m-auto">
-        <div className="text-pale text-[30px] mt-[150px] mb-[50px] text-center">THE CORE FEATURES</div>
-        <div className="flex gap-[40px]">
-          <div className="bg-dark w-[270px] h-[384px] flex flex-col items-center p-[20px] pt-[30px]">
+        <div className="text-pale text-[30px] mt-[150px] mb-[50px] text-center font-['IntegralCF']">
+          THE CORE FEATURES
+        </div>
+        <div className="flex gap-[40px] overflow-auto ">
+          <div className="bg-dark w-[270px] h-[384px] shrink-0 flex flex-col items-center p-[20px] pt-[30px] ">
             <Image
               src="/feature-nfts.webp"
               alt="arrow right"
@@ -17,11 +19,20 @@ const CoreFeaturesSection = () => {
               priority
               style={{ objectFit: "contain" }}
             />
-            <div className="text-white uppercase text-[24px] font-bold mt-[20px]">NFTs</div>
-            <div className="text-light-grey text-[16px] my-[15px] text-center">Buy mystery boxes or trade hot NFTs on our Launchpad & marketplace.</div>
-            <div className="text-light-grey text-[16px] text-center">Collaborations with gaming studios, eSporters, gamer girls & historical events connected to gaming hosting their NFT collections.</div>
+            <div className="text-white uppercase text-[24px] font-bold mt-[20px] font-['IntegralCF']">
+              NFTs
+            </div>
+            <div className="text-light-grey text-[16px] my-[15px] text-center font-['Industry']">
+              Buy mystery boxes or trade hot NFTs on our Launchpad &
+              marketplace.
+            </div>
+            <div className="text-light-grey text-[16px] text-center font-['Industry']">
+              Collaborations with gaming studios, eSporters, gamer girls &
+              historical events connected to gaming hosting their NFT
+              collections.
+            </div>
           </div>
-          <div className="bg-dark w-[270px] h-[384px] flex flex-col items-center p-[20px] pt-[30px]">
+          <div className="bg-dark w-[270px] h-[384px] shrink-0 flex flex-col items-center p-[20px] pt-[30px]">
             <Image
               src="/feature-play.webp"
               alt="arrow right"
@@ -31,10 +42,16 @@ const CoreFeaturesSection = () => {
               priority
               style={{ objectFit: "contain" }}
             />
-            <div className="text-white uppercase text-[24px] font-bold mt-[20px]">PLAY TO EARN</div>
-            <div className="text-light-grey text-[16px] my-[15px] text-center">Earn <span className='text-electric-lime'>$HYPER</span> by participating in our online buy-in tournaments, high-stake 1 v 1 battles, or playing crypto games.</div>
+            <div className="text-white uppercase text-[24px] font-bold mt-[20px] font-['IntegralCF']">
+              PLAY TO EARN
+            </div>
+            <div className="text-light-grey text-[16px] my-[15px] text-center font-['Industry']">
+              Earn <span className="text-electric-lime">$HYPER</span> by
+              participating in our online buy-in tournaments, high-stake 1 v 1
+              battles, or playing crypto games.
+            </div>
           </div>
-          <div className="bg-dark w-[270px] h-[384px] flex flex-col items-center p-[20px] pt-[30px]">
+          <div className="bg-dark w-[270px] h-[384px] shrink-0 flex flex-col items-center p-[20px] pt-[30px]">
             <Image
               src="/controller.svg"
               alt="Game Controller"
@@ -44,11 +61,19 @@ const CoreFeaturesSection = () => {
               priority
               style={{ objectFit: "contain" }}
             />
-            <div className="text-white uppercase text-[24px] font-bold mt-[26px]">GAME STORE</div>
-            <div className="text-light-grey text-[16px] my-[15px] text-center">Explore our game store for the hottest play-to-earn crypto and gambling games.</div>
-            <div className="text-light-grey text-[16px] text-center">Some games developed by HyperchainX, others by our partner studios.</div>
+            <div className="text-white uppercase text-[24px] font-bold mt-[26px] font-['IntegralCF']">
+              GAME STORE
+            </div>
+            <div className="text-light-grey text-[16px] my-[15px] text-center font-['Industry']">
+              Explore our game store for the hottest play-to-earn crypto and
+              gambling games.
+            </div>
+            <div className="text-light-grey text-[16px] text-center font-['Industry']">
+              Some games developed by HyperchainX, others by our partner
+              studios.
+            </div>
           </div>
-          <div className="bg-dark w-[270px] h-[384px] flex flex-col items-center p-[20px] pt-[30px]">
+          <div className="bg-dark w-[270px] h-[384px] shrink-0 flex flex-col items-center p-[20px] pt-[30px]">
             <Image
               src="/feature-live-stream.webp"
               alt="arrow right"
@@ -58,9 +83,16 @@ const CoreFeaturesSection = () => {
               priority
               style={{ objectFit: "contain" }}
             />
-            <div className="text-white uppercase text-[24px] font-bold mt-[26px]">LIVE STREAMS</div>
-            <div className="text-light-grey text-[16px] my-[15px] text-center">Stream or watch any game on our app.</div>
-            <div className="text-light-grey text-[16px] text-center">Streamers can monetize their content and receive $HYPER donations and gifts.</div>
+            <div className="text-white uppercase text-[24px] font-bold mt-[26px] font-['IntegralCF']">
+              LIVE STREAMS
+            </div>
+            <div className="text-light-grey text-[16px] my-[15px] text-center font-['Industry']">
+              Stream or watch any game on our app.
+            </div>
+            <div className="text-light-grey text-[16px] text-center font-['Industry']">
+              Streamers can monetize their content and receive $HYPER donations
+              and gifts.
+            </div>
           </div>
         </div>
       </div>
