80eba18ba41577010672336b251e352c9b1a281b
cardoso-topdev <oscar@cardoso-devstar.com>
2023-05-03 23:27:12 -0600

    complete MultiChainSection component for mobile responsive

--- app/components/sections/MultiChainSection.tsx
+++ app/components/sections/MultiChainSection.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 
 const MultiChainSection = () => {
   return (
-    <section>
+    <section className="px-[30px]">
       <div className="max-w-[1200px] m-auto">
         <div className="flex flex-col items-center mt-[100px]">
           <Image
@@ -15,8 +15,10 @@ const MultiChainSection = () => {
             priority
             style={{ objectFit: "contain" }}
           />
-          <div className="text-pale text-[30px] text-center absolute translate-y-[290px]">MULTI-CHAIN</div>
-          <div className="text-electric-lime text-[20px] whitespace-pre-line text-center">{`Unleash the full potential of your NFT collection with our multi-chain marketplace - accessible,
+          <div className="text-pale text-[30px] text-center absolute translate-y-[290px] max-xmd:translate-y-[260px] font-['IntegralCF']">
+            MULTI-CHAIN
+          </div>
+          <div className="text-electric-lime text-[20px] max-lg:text-[16px] max-lg:whitespace-normal whitespace-pre-line text-center font-['Industry']">{`Unleash the full potential of your NFT collection with our multi-chain marketplace - accessible,
           versatile, and always adapting to your needs.`}</div>
         </div>
       </div>
